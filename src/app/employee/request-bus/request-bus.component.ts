c0f333e091a9c961bd4df59a8da3e570dcce3a1b
Akshay Akkati <AA00490096@techmahindra.com>
2018-02-08 20:55:52 +0530

    modified request bus page

--- src/app/employee/request-bus/request-bus.component.ts
+++ src/app/employee/request-bus/request-bus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-request-bus',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-bus.component.css']
 })
 export class RequestBusComponent implements OnInit {
+  public registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+    private buildForm(): void {
+      this.registerForm = this._formBuilder.group({
+        'Journey City': ['', [Validators.required]],
+        'Journey Location': ['', [Validators.required]],
+        'Contact Number': ['', [Validators.required]],
+        'Journey Type': ['', [Validators.required]],
+        'Route No': ['', [Validators.required]],
+        'Pickup Point': ['', [Validators.required]]
+      });
   }
 
 }
