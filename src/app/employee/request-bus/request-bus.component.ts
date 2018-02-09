b74d14b0984b495280f89e291212a06e0739400f
Akshay Akkati <AA00490096@techmahindra.com>
2018-02-09 20:16:17 +0530

    json

--- src/app/employee/request-bus/request-bus.component.ts
+++ src/app/employee/request-bus/request-bus.component.ts
@@ -10,38 +10,40 @@ export class RequestBusComponent implements OnInit {
   public registerForm: FormGroup;
   public showme: boolean = false;
   public pickupPoint: any;
-  public json1: JSON;
-  public routes_list : [{
+  public x: any;
+  public y: any;
+  public routes_list: [{
     RouteNo: string,
-    Origin : string,
-    Destination : string,
-    DepartuteTime : string,
+    Origin: string,
+    Destination: string,
+    DepartuteTime: string,
     PickupPoint: any
-}];
+  }];
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.buildForm();
     this.routes_list = [{
       RouteNo: "1",
-      Origin : "TMEC",
-      Destination : "Nagalanda Circle",
-      DepartuteTime : "8:50 AM",
+      Origin: "TMEC",
+      Destination: "Nagalanda Circle",
+      DepartuteTime: "8:50 AM",
       PickupPoint: ["Nagalanda Junction", "Madiwala", "Silk Board"]
-  },
-  {
+    },
+    {
       RouteNo: "2",
-      Origin : "TMEC",
-      Destination : "Konappana Agrahara",
-      DepartuteTime : "9:30 AM",
+      Origin: "TMEC",
+      Destination: "Konappana Agrahara",
+      DepartuteTime: "9:30 AM",
       PickupPoint: ["Ecity", "BTM", "sarjapur"]
-  }]
+    }]
   }
-  togglepickpoint(pickpoint1){
+  togglepickpoint(pickpoint1) {
     this.pickupPoint = pickpoint1;
   }
   onSelect(selectedItem: any) {
-    console.log(selectedItem);
+    this.x = JSON.stringify(selectedItem);
+    console.log(this.x);
     console.log("Route No: ", selectedItem.RouteNo);
     console.log("Origin: ", selectedItem.Origin);
     console.log("Destination: ", selectedItem.Destination);
@@ -51,27 +53,25 @@ export class RequestBusComponent implements OnInit {
   public register(model) {
     console.log(model);
   }
-  showpanel(){
+  showpanel() {
     this.showme = true;
   }
   reset() {
     this.showme = false;
   }
-
-    private buildForm(): void {
-      this.registerForm = this._formBuilder.group({
-        'gid': ['4900', [Validators.required]],
-        'emp_name': ['empname', [Validators.required]],
-        'gender': ['male', [Validators.required]],
-        'journeycity': ['', [Validators.required]],
-        'journeylocation': ['', [Validators.required]],
-        'ContactNumber': ['', [Validators.required]],
-        'ticket_type': ['', [Validators.required]],
-        'Route No': ['', [Validators.required]],
-        // 'Pickup Point': ['', [Validators.required]]
-      });
+  private buildForm(): void {
+    this.registerForm = this._formBuilder.group({
+      'gid': ['4900', [Validators.required]],
+      'emp_name': ['empname', [Validators.required]],
+      'gender': ['male', [Validators.required]],
+      'journeycity': ['', [Validators.required]],
+      'journeylocation': ['', [Validators.required]],
+      'ContactNumber': ['', [Validators.required]],
+      'ticket_type': ['', [Validators.required]],
+      // 'Route_No': ['', [Validators.required]],
+      // 'Pickup Point': ['', [Validators.required]]
+    });
   }
-  
 }
-  
+
 
