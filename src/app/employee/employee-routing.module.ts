6f6f7ed434ed6d66637e94948050442e27afa321
TransportTechm <p2pr.digital@gmail.com>
2018-02-12 19:07:13 +0530

    Added Two pages Cancel page View History

--- src/app/employee/employee-routing.module.ts
+++ src/app/employee/employee-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { StartComponent } from '../shared/start/start.component';
 import { RequestBusComponent } from './request-bus/request-bus.component';
 import { ViewAvailabilityComponent } from './view-availability/view-availability.component';
+import { ViewHistoryComponent } from './view-history/view-history.component';
+import { CancelRequestComponent } from './cancel-request/cancel-request.component';
 
 const employeeroutes: Routes = [
   {
     path: '',
     children: [
       {path: 'requestbus',component: RequestBusComponent},
-      {path: 'availability',component: ViewAvailabilityComponent}
+      {path: 'availability',component: ViewAvailabilityComponent},
+      {path: 'viewhistory',component: ViewHistoryComponent},
+      {path: 'cancelrequest',component: CancelRequestComponent},
     ]
   }
 ];
--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -6,6 +6,8 @@ import { SharedModule } from '../shared/shared.module';
 import { RequestBusComponent } from './request-bus/request-bus.component';
 import { ViewAvailabilityComponent } from './view-availability/view-availability.component';
 import { RequestBusService } from './services/request-bus.service';
+import { ViewHistoryComponent } from './view-history/view-history.component';
+import { CancelRequestComponent } from './cancel-request/cancel-request.component';
 
 
 @NgModule({
@@ -14,7 +16,7 @@ import { RequestBusService } from './services/request-bus.service';
     SharedModule,
     EmployeeRoutingModule
   ],
-  declarations: [RequestBusComponent, ViewAvailabilityComponent],
+  declarations: [RequestBusComponent, ViewAvailabilityComponent, ViewHistoryComponent, CancelRequestComponent],
   providers: [
     RequestBusService
   ]
