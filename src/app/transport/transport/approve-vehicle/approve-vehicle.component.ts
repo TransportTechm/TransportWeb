d3472ac94b8aed25c4e4462596196da09aaca07c
TransportTechm <p2pr.digital@gmail.com>
2018-02-15 16:44:16 +0530

    Added Approve Vehicle page

---
+++ src/app/transport/transport/approve-vehicle/approve-vehicle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-approve-vehicle',
+  templateUrl: './approve-vehicle.component.html',
+  styleUrls: ['./approve-vehicle.component.css']
+})
+export class ApproveVehicleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
