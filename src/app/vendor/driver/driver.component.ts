85273706fcb75041ad4e7fb0703679cbc549a599
Akshay Akkati <AA00490096@techmahindra.com>
2018-02-20 15:39:04 +0530

    driver registration page

--- src/app/vendor/driver/driver.component.ts
+++ src/app/vendor/driver/driver.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, NgModule } from '@angular/core';
+import { Http } from '@angular/http';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder, FormArray, FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-driver',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./driver.component.css']
 })
 export class DriverComponent implements OnInit {
+  public driverForm: FormGroup;
 
-  constructor() { }
+  constructor(private http: Http, private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+  private buildForm(): void {
+    this.driverForm = this._formBuilder.group({
+      'driverName': ['', [Validators.required]],
+      'address': ['', [Validators.required]],
+      'mobileNumber': ['', [Validators.required]],
+      'licenseNumber': ['', [Validators.required]]
+    });
+  }
+
+  public register(model) {
+    console.log(model)
   }
 
 }
