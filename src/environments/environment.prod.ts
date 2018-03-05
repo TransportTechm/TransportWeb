5c8b4daebc661e755f59fc74718aef6681a92acf
TransportTechm <36197874+TransportTechm@users.noreply.github.com>
2018-03-05 21:24:47 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   employeeServiceUrl: 'https://employeeservice-techm-prod.cfapps.io/employee/1.0/',
-  transportServiceUrl: 'https://transportservicep.cfapps.io/1.0/',
+  transportServiceUrl: 'https://zuulgatewayp.cfapps.io/api/transport/1.0/',
   vendorServiceUrl: 'https://zuulgatewayp.cfapps.io/api/vendor/1.0/'
 
 };
